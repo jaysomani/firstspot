6b98d0653b2161cf9c5be75059ae84255d8af54b
stharanzn <38763905+stharanzn@users.noreply.github.com>
2022-10-08 15:40:40 +0530

    Add files via upload

--- contact/contact.js
+++ contact/contact.js
@@ -1,5 +1,11 @@
+const sendLetter = document.getElementById("sendLetter");
+
+
 function addClass() {
     document.body.classList.add("sent");
+    
+    
 }
+
 
 sendLetter.addEventListener("click", addClass);
