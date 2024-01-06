00ca1f210f06298af2f97fce532e17f084edd281
Manish <manishkr061210@gmail.com>
2024-01-07 00:32:34 +0530

    Adding custom cursor

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 "use strict";
+
+var crsr = document.querySelector("#cursor");
+var blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+
+
+
+
 (function () {
 	// Global variables
 	var userAgent = navigator.userAgent.toLowerCase(),
