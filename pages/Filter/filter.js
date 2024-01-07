8fa5f414a7bfa3602dcb25c12af5ca8e4d27346b
Suhani2804 <suhanigupta28@gmail.com>
2024-01-07 10:57:19 +0530

    hover effect on buttons and card added

--- pages/Filter/filter.js
+++ pages/Filter/filter.js
@@ -87,22 +87,23 @@ let places = {
             placeName: "Pune",
             category: "South",
             price: "15,000",
-            image: "mumbai_tour.jpg",
+            image: "pune_tour.jpg",
         },
         {
             placeName: "Kolapur",
             category: "South",
             price: "15,000",
-            image: "mumbai_tour.jpg",
+            image: "kolapur_tour.jpg",
         },
         {
             placeName: "Roorkee",
             category: "North",
             price: "15,000",
-            image: "mumbai_tour.jpg",
+            image: "roorkee_tour.jpg",
         },
     ],
 };
+
 for (let i of places.data) {
     //Create Card
     let card = document.createElement("div");
@@ -168,30 +169,18 @@ document.getElementById("search").addEventListener("click", () => {
     let searchInput = document.getElementById("search-input").value;
     let elements = document.querySelectorAll(".place-name");
     let cards = document.querySelectorAll(".card");
-    let found = false;
     //loop through all elements
     elements.forEach((element, index) => {
         //check if text includes the search value
         if (element.innerText.includes(searchInput.toUpperCase())) {
             //display matching card
             cards[index].classList.remove("hide");
-            found = true; 
         } else {
             //hide others
             cards[index].classList.add("hide");
-            // document.getElementById("places").innerHTML="Not Found";
         }
     });
-if (!found) {
-    alert("No matching results found");
-    // You can replace the alert with your preferred method of displaying a "not found" message
-}
 });
-
-
-
-
-
 //Initially display all places
 window.onload = () => {
     filterPlace("all");
