2e883a73a0878b65983f4d756042c98ad6190431
Kapileswar-Moharana <kapileswarmoharana32161@gmail.com>
2024-01-09 15:23:10 +0530

    Add Reverse Home And Make The Page Responsive

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,11 +1,11 @@
 const sendLetter = document.getElementById("sendLetter");
 
-
 function addClass() {
     document.body.classList.add("sent");
-    
-    
 }
 
-
 sendLetter.addEventListener("click", addClass);
+
+function goToHome() {
+    window.location.href = "../../index.html";
+}
