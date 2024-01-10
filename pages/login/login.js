a0c9f3d7f810e2d2fd3f0f86096f9708921484e1
VinayCheripally <vinaycheripally1@gmail.com>
2024-01-10 23:40:13 +0530

    popup_msg_for_no_username_or_password

--- pages/login/login.js
+++ pages/login/login.js
@@ -2,25 +2,29 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebas
 // import { initializeApp } from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHLP7isomKx1zKwle0PpydCYdhHA1ctM4",
-    authDomain: "firstspot-b7b7b.firebaseapp.com",
-    databaseURL: "https://firstspot-b7b7b-default-rtdb.firebaseio.com",
-    projectId: "firstspot-b7b7b",
-    storageBucket: "firstspot-b7b7b.appspot.com",
-    messagingSenderId: "781962837914",
-    appId: "1:781962837914:web:6662ae66f885e764e2c423"
+  apiKey: "AIzaSyAHLP7isomKx1zKwle0PpydCYdhHA1ctM4",
+  authDomain: "firstspot-b7b7b.firebaseapp.com",
+  databaseURL: "https://firstspot-b7b7b-default-rtdb.firebaseio.com",
+  projectId: "firstspot-b7b7b",
+  storageBucket: "firstspot-b7b7b.appspot.com",
+  messagingSenderId: "781962837914",
+  appId: "1:781962837914:web:6662ae66f885e764e2c423",
 };
 
 // Initialize Firebase
 //const app = initializeApp(firebaseConfig);
 const app = initializeApp(firebaseConfig);
 
-import { getDatabase, ref, set, child, get }
-from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  set,
+  child,
+  get,
+} from "https://www.gstatic.com/firebasejs/9.6.11/firebase-database.js";
 // import { validation } from "uuid";
 const db = getDatabase();
 // ////////The refrences////////////////////////////////////////////////
-
 
 const username = document.getElementById("userInp");
 const pass = document.getElementById("passInp");
@@ -29,25 +33,23 @@ const submit = document.getElementById("sub_btn");
 //////////////////
 
 function AuthenticateUser() {
-    const dbref = ref(db);
-    console.log(username.value);
-    get(child(dbref, "UsersList/" + username.value)).then((snapshot) => {
-        // console.log(snapshot.val());
-        if (snapshot.exists()) {
-            // let dbpass = decPass(snapshot.val().password);
-            // console.log(dbpass, pass.value, "here");
-            let dbpass = snapshot.val().password;
-            if (dbpass == pass.value) {
-                login(username.value);
-            } else {
-                alert("user does not exist");
-            }
-        } else {
-
-            alert("username or password is invalid");
-        }
-
-    });
+  const dbref = ref(db);
+  console.log(username.value);
+  get(child(dbref, "UsersList/" + username.value)).then((snapshot) => {
+    // console.log(snapshot.val());
+    if (snapshot.exists()) {
+      // let dbpass = decPass(snapshot.val().password);
+      // console.log(dbpass, pass.value, "here");
+      let dbpass = snapshot.val().password;
+      if (dbpass == pass.value) {
+        login(username.value);
+      } else {
+        alert("user does not exist");
+      }
+    } else {
+      alert("username or password is invalid");
+    }
+  });
 }
 
 // ////////////decript process
@@ -59,21 +61,20 @@ function AuthenticateUser() {
 //////////////////login
 
 function login(user) {
-    // let keepLoggedIn = document.getElementById("customSwitch1").checked;
-    // if (!keepLoggedIn) {
-    //     sessionStorage.setItem("user", user);
-    //     window.location = "../home.html";
-    //     // console.log(sessionStorage.getItem("user"));
-    // } else {
-    //     localStorage.setItem("keepLoggedIn", "yes");
-    //     localStorage.setItem("user", user);
-    //     window.location = "../home.html";
-    //     // console.log(localStorage.getItem("user"));
-    // }
+  // let keepLoggedIn = document.getElementById("customSwitch1").checked;
+  // if (!keepLoggedIn) {
+  //     sessionStorage.setItem("user", user);
+  //     window.location = "../home.html";
+  //     // console.log(sessionStorage.getItem("user"));
+  // } else {
+  //     localStorage.setItem("keepLoggedIn", "yes");
+  //     localStorage.setItem("user", user);
+  //     window.location = "../home.html";r;
+  //     // console.log(localStorage.getItem("user"));
+  // }
 
-    sessionStorage.setItem("user", user);
-    window.location = "../../index.html";
-
+  sessionStorage.setItem("user", user);
+  window.location = "../../index.html";
 }
 // ////////////////////submit
 
